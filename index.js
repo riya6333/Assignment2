9deb69c26ba5f5643e0b55ea230929fb9c1383c6
Riya Yadav <riya.yadav@smartData-305.local>
2023-10-25 12:40:04 +0530

    Add Assignment2

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import Assignment2 from './Assignment2';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => Assignment2);
